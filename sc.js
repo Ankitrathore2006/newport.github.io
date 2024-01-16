4a7259261633f3fc17cbe0f2107dffadaddfabc0
Ankitrathore2006 <136782863+Ankitrathore2006@users.noreply.github.com>
2024-01-16 19:46:05 +0530

    Add files via upload

--- sc.js
+++ sc.js
@@ -12,3 +12,28 @@ icon.addEventListener('click', function() {
   nav.classList.toggle('show');
   blue.classList.toggle('slide');
 });
+
+
+
+
+
+window.addEventListener("scroll", reveal);
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+
